038b27f3a701f2bce903f4fb9fced1ea04266190
VilgelmTrigger <92038769+VilgelmTrigger@users.noreply.github.com>
2022-02-25 14:18:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,11 @@
 import telebot
-import time
-from time import sleep
 from telebot import types
 import sqlite3
-import sys
-import pathlib
 
 TOKEN = '5233219459:AAHdU6Db-F4ZnTfay_2MZIMKWCP-rsjPuqA' #token bota in telegram
 bot = telebot.TeleBot(TOKEN)
 
 #подключаю базу данных SQLite3 к своему проекту
-connect = sqlite3.connect('telegramBD.db')
-cursor = connect.cursor()
 ID_CHAT = -1001756928504
 
 #текст после написания start
@@ -19,10 +13,10 @@ ID_CHAT = -1001756928504
 def text(message):
 
     #элементы главного меню
-    markup = types.ReplyKeyboardMarkup(resize_keyboard= True) 
+    markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
     item1 = types.KeyboardButton('🔥 F.A.Q.')
     item2 = types.KeyboardButton('🔧 Задать вопрос')
-    item3 = types.KeyboardButton('❗ Сообщить об ошибке') 
+    item3 = types.KeyboardButton('❗ Сообщить об ошибке')
     markup.add(item1, item2, item3)
 
     #вывод самого сообщения
@@ -32,7 +26,7 @@ def text(message):
 def text_q(message):
    if message.text == "🔧 Задать вопрос":
     #элементы главного меню
-    markup = types.ReplyKeyboardMarkup(resize_keyboard= True) 
+    markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
     item1 = types.KeyboardButton('◀️ Назад')
     markup.add(item1)
 
@@ -40,23 +34,22 @@ def text_q(message):
     sent = bot.send_message(message.chat.id, 'Напишите текст вопроса!', reply_markup = markup)
     bot.register_next_step_handler(sent,blup)
 
-   elif message.text == "❗ Cообщить об ошибке": 
+   elif message.text == "❗ Cообщить об ошибке":
     #элементы главного меню
-    markup = types.ReplyKeyboardMarkup(resize_keyboard= True) 
+    markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
     item1 = types.KeyboardButton('◀️ Назад')
     markup.add(item1)
     #вывод самого сообщения
     sent = bot.send_message(message.chat.id, 'Опишите ошибку с которой вы столкнулись.', reply_markup = markup)
     bot.register_next_step_handler(sent,bug)
 
-   elif message.text == "🔥 F.A.Q.": 
+   elif message.text == "🔥 F.A.Q.":
     #элементы главного меню
-    markup = types.ReplyKeyboardMarkup(resize_keyboard= True) 
+    markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
     item1 = types.KeyboardButton('◀️ Назад')
     markup.add(item1)
-    
-    script_path = pathlib.Path(sys.argv[0]).parent  # абсолютный путь до каталога, где лежит скрипт
-    connect = sqlite3.connect(script_path / "telegramBD.db")  # формируем абсолютный путь до файла базы
+
+    connect = sqlite3.connect('telegramBD.db', check_same_thread=False)
     cursor = connect.cursor()
 
     #вывод из базы данных всех вопрос и ответов
@@ -69,12 +62,12 @@ def text_q(message):
     i = 0
     while i < min(len(A), len(Q)):
      message_str += f"Q: {Q[i]}\nA: {A[i]}\n\n"
-     i += 1 
+     i += 1
     bot.send_message(message.chat.id, "Вот вопросы которые часто задают пользователи: \n\n" + message_str, reply_markup = markup)
 
-   elif message.text == "◀️ Назад": 
+   elif message.text == "◀️ Назад":
     #элементы главного меню
-    markup = types.ReplyKeyboardMarkup(resize_keyboard= True) 
+    markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
     item1 = types.KeyboardButton('🔥 F.A.Q.')
     item2 = types.KeyboardButton('🔧 Задать вопрос')
     item3 = types.KeyboardButton('❗ Cообщить об ошибке')
@@ -90,28 +83,28 @@ def blup(message):
  user_id = message.from_user.id
 
  bot.send_message(ID_CHAT, 'Новое уведомление! \n Пользователь задал вам вопрос. Проверьте БД и ответьте на него!\n\n' + name)
- script_path = pathlib.Path(sys.argv[0]).parent  # абсолютный путь до каталога, где лежит скрипт
- connect = sqlite3.connect(script_path / "telegramBD.db")  # формируем абсолютный путь до файла базы
+ connect = sqlite3.connect('telegramBD.db', check_same_thread=False)
  cursor = connect.cursor()
- cursor.execute("INSERT INTO users (User_heshtag) VALUES (?)", (user_id,))
+ cursor = connect.cursor()
+ cursor.execute("INSERT OR IGNORE INTO users (User_heshtag) VALUES (?)", (user_id,))
  cursor.execute("INSERT INTO question (Text,User_heshtag,username) VALUES (?,?,?)", (name,user_id,username,))
  connect.commit()
- connect.close()
 
  bot.send_message(message.chat.id, 'Я отправил ваш запрос админам, ожидайте')
-    
+ connect.close()
+
 def bug(message):
  name=message.text
  username = message.from_user.username
 
  bot.send_message(ID_CHAT, 'Новое уведомление! \n Пользователь обнаружил новую ошибку в игре! Нужно обозначить тип ошибки в БД! Вот текст сообщения:\n\n' + name)
- script_path = pathlib.Path(sys.argv[0]).parent  # абсолютный путь до каталога, где лежит скрипт
- connect = sqlite3.connect(script_path / "telegramBD.db")  # формируем абсолютный путь до файла базы
+ connect = sqlite3.connect('telegramBD.db', check_same_thread=False)
+ cursor = connect.cursor()
  cursor = connect.cursor()
  cursor.execute("INSERT INTO Bugs (User_Text,username) VALUES (?,?)", (name,username,))
  connect.commit()
- connect.close()
 
  bot.send_message(message.chat.id, 'Сообщение об ошибке было отправленно разработчикам! Спасибо за вашу наблюдательность!')
+ connect.close()
 
 bot.polling(none_stop=True)
